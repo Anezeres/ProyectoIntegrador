dadb07b263a73c3f9f1bc7f9ab7ff984237a29b5
Juan David (iron-mind) <davidtovarmontoya@gmail.com>
2023-11-05 17:53:01 -0500

    landing and some fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import { createRoot } from "react-dom/client";
-import Experience from "./Experience";
 import "./styles.css";
-import { Canvas } from "@react-three/fiber";
 import App from "./app.js";
+import React from "react";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-	<>
-		<Canvas camera={{ position: [-1, 4, 7] }}>
-			<Experience />
-		</Canvas>
-
-		{/* TODO LO QUE NO ES 3D */}
+	<React.StrictMode>
 		<App />
-	</>
+	</React.StrictMode>
 );
